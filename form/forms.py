3aa3b419c6c8868ae5d9e6075b02b5bc3664d8cb
Bruno Koura <brunokoura@gmail.com>
2022-10-27 08:38:20 -0300

    adding new fields into the forms

--- form/forms.py
+++ form/forms.py
@@ -13,6 +13,8 @@ component_type = (
 
 
 class Form(forms.Form):
+    branchName = forms.CharField(widget=forms.Textarea(attrs={"rows": 1,"cols":50}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"cols": 50}))
     type = forms.ChoiceField(choices=material_type, initial="Select One Option")
     serialNumber = forms.CharField(label='Serial Number', required=False)
     itemNumber = forms.IntegerField(label='Item Number', required=False)
@@ -20,11 +22,13 @@ class Form(forms.Form):
     phase = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Monophasic'}))
     capacity = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: 15cv'}))
     centerQuantity = forms.IntegerField(label='Center Quantity')
-    componentSelection = forms.ChoiceField(choices=component_type, label='Component Selection')
-    componentQuantity = forms.IntegerField(label='Component Quantity')
+    componentSelection = forms.ChoiceField(choices=component_type, label='Component Selection', required=True)
+    componentQuantity = forms.IntegerField(label='Component Quantity', required=True)
     componentSelection1 = forms.ChoiceField(choices=component_type, label='Component Selection', required=False)
     componentQuantity1 = forms.IntegerField(label='Component Quantity,', required=False)
     componentSelection2 = forms.ChoiceField(choices=component_type, label='Component Selection', required=False)
     componentQuantity2 = forms.IntegerField(label='Component Quantity', required=False)
+    componentSelection3 = forms.ChoiceField(choices=component_type, label='Component Selection', required=False)
+    componentQuantity3 = forms.IntegerField(label='Component Quantity', required=False)
     commentary = forms.CharField(widget=forms.Textarea(attrs={"rows":8, "cols":80}))
 
