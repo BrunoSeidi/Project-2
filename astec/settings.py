34617c03fa2a7683f8cf6c1656897dc36efb87f0
Bruno Koura <brunokoura@gmail.com>
2022-10-31 09:43:24 -0300

    Image

--- astec/settings.py
+++ astec/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = 'astec.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.oracle',
@@ -149,6 +148,9 @@ STATICFILES_DIRS = [
     BASE_DIR/"static"
 ]
 STATIC_ROOT = "/static"
+
+MEDIA_ROOT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- form/urls.py
+++ form/urls.py
@@ -1,8 +1,9 @@
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+    
+]+staticfiles_urlpatterns()
