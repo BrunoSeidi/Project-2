9b204f6c2c40e22be0c0a9895e2474126a4a8024
Bruno Koura <brunokoura@gmail.com>
2022-11-04 09:27:09 -0300

    teste

--- form/forms.py
+++ form/forms.py
@@ -4,16 +4,16 @@ from .component import component_type, material_type
 
 
 class Form(forms.Form):
-    branchName = forms.CharField(widget=forms.TextInput(attrs={"size":50}))
+    branchName = forms.CharField(widget=forms.TextInput(attrs={"size": 50}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"size": 40}))
-    name = forms.CharField(widget=forms.TextInput(attrs={"size":50}))
+    name = forms.CharField(widget=forms.TextInput(attrs={"size": 50}))
     
     type = forms.ChoiceField(choices=material_type, initial="Select One Option")
     serialNumber = forms.CharField(label='Serial Number', required=False)
     itemNumber = forms.IntegerField(label='Item Number', required=False)
-    date = forms.DateField(label='Manufactoring Date', widget=forms.DateInput(attrs={'placeholder':'Ex: 10/05/2022'}))
+    date = forms.DateField(widget=forms.DateInput(attrs={'placeholder':'Ex: 10/05/2022'}), required=False)
     phase = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: SINGLE PHASE'}))
-    capacity = forms.CharField(label='Output Power', widget=forms.TextInput(attrs={'placeholder': 'Ex: 15cv'}))
+    capacity = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: 15cv'}))
     centerQuantity = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '2'}))
 
     componentSelection = forms.ChoiceField(choices=component_type, label='Component Selection', required=True)
@@ -27,4 +27,7 @@ class Form(forms.Form):
     componentSelection4 = forms.ChoiceField(choices=component_type, label='Component Selection', required=False)
     componentQuantity4 = forms.IntegerField(label='Component Quantity', required=False)
     commentary = forms.CharField(widget=forms.Textarea(attrs={"rows":8, "cols":200}))
+
+
+
 
